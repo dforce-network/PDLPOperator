6da13b15da1dbacfbd9f478d453af8a962409aaa
Huimin HE <joyioushe@gmail.com>
2022-03-08 19:44:31 +0800

    :new: lending submodule for integration

---
+++ contracts/mock/Lending.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "../../lending/contracts/msd/MSD.sol";
+import "../../lending/contracts/msd/MSDControllerV2.sol";
+import "../../lending/contracts/Controller.sol";
+import "../../lending/contracts/iToken.sol";
